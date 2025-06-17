2c4a3f7f73e5fe57fbe31215628b375a810629e4
tariqk <tariqk@axioned.com>
2025-06-17 19:29:30 +0530

    setup

--- components/main-section.tsx
+++ components/main-section.tsx
@@ -8,12 +8,12 @@ const MainSection = () => {
         src={
           "https://images.unsplash.com/photo-1558981403-c5f9899a28bc?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         }
-        alt="Background"
+        alt="Background image"
         fill
         className="object-cover"
         priority
       />
-      <div className="overlay  inset-0  bg-primary opacity-50 z-10"></div>
+      <div className="overlay  inset-0  bg-primary opacity-70 z-10"></div>
       <div className="overlay inset-0 z-20 flex flex-col items-center justify-center text-white">
         <p className="font-sans">The Latest Product from ecoshop</p>
         <div className="mt-16 flex flex-col items-center justify-center">
